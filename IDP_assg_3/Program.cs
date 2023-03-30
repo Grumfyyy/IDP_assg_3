7c65d9182285867f98a442255081f820f962ac2b
Grumfyyy <swadijosef@gmail.com>
2023-03-30 09:23:41 +0200

    Första commiten

---
+++ IDP_assg_3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace IDP_assg_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Element
+    {
+        public string Name;
+        public int Z;
+        public string Type;
+        public int meltingPoint;
+        public int boilingPoint;
+    }
+}
