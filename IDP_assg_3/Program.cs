fe79b55dd1d345b7eb52b4d06e45b5202b8287f9
Grumfyyy <swadijosef@gmail.com>
2023-03-30 09:30:14 +0200

    Bytte från engelska, till svenska, samt instansiera tre stycken grundämnen : syre, järn och guld

--- IDP_assg_3/Program.cs
+++ IDP_assg_3/Program.cs
@@ -4,16 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+
+        private static void Uppgift2()
+        {
+            Grundämne oxygen = new Grundämne() { namn = "Syre", Z = 8, typ = "ickemetall", smältpunkt = 54.36, kokpunkt = 90.188 };
+            Grundämne järn = new Grundämne() { namn = "Järn", Z = 26, typ = "metall", smältpunkt = 1811, kokpunkt = 3134};
+            Grundämne guld = new Grundämne() { namn = "Guld", Z = 8, typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243 };
+        }
+
     }
 
-    public class Element
+    public class Grundämne
     {
-        public string Name;
+        public string namn;
         public int Z;
-        public string Type;
-        public int meltingPoint;
-        public int boilingPoint;
+        public string typ;
+        public double smältpunkt;
+        public double kokpunkt;
     }
 }
