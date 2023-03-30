b06f89f8cca0ae2925d020a25d9d631dfb2eac14
Grumfyyy <swadijosef@gmail.com>
2023-03-30 09:34:40 +0200

    korrigerade lite variabler, samt flyttade så att alla uppgifter utförs i Main funktionen, samt la till Print funktionen i Grundämne klassen.

--- IDP_assg_3/Program.cs
+++ IDP_assg_3/Program.cs
@@ -4,14 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+            //Uppgift 2
+            Grundämne syre = new Grundämne() { namn = "Syre", Z = 8, typ = "Ickemetall", smältpunkt = 54.36, kokpunkt = 90.188 };
+            Grundämne järn = new Grundämne() { namn = "Järn", Z = 26, typ = "Metall", smältpunkt = 1811, kokpunkt = 3134 };
+            Grundämne guld = new Grundämne() { namn = "Guld", Z = 8, typ = "Metall", smältpunkt = 1337.33, kokpunkt = 3243 };
 
-        private static void Uppgift2()
-        {
-            Grundämne oxygen = new Grundämne() { namn = "Syre", Z = 8, typ = "ickemetall", smältpunkt = 54.36, kokpunkt = 90.188 };
-            Grundämne järn = new Grundämne() { namn = "Järn", Z = 26, typ = "metall", smältpunkt = 1811, kokpunkt = 3134};
-            Grundämne guld = new Grundämne() { namn = "Guld", Z = 8, typ = "metall", smältpunkt = 1337.33, kokpunkt = 3243 };
+            //Uppgift 3
+            syre.Print();
+            järn.Print();
+            guld.Print();
         }
 
     }
@@ -23,5 +24,13 @@
         public string typ;
         public double smältpunkt;
         public double kokpunkt;
+
+        public void Print()
+        {
+            Console.WriteLine($"Grundämne : {this.namn}");
+            Console.WriteLine($"Typ : {this.typ}");
+            Console.WriteLine($"Smältpunkt : {this.smältpunkt} K");
+            Console.WriteLine($"Kokpunkt : {this.kokpunkt} K");
+        }
     }
 }
